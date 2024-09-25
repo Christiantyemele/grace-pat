876fbc740d357e03e57e5ad87528dec71418e993
Christian_Yemele <yemelechristian2@gmail.com>
2024-09-25 18:39:38 +0100

    working on database

--- src/database/queries.rs
+++ src/database/queries.rs
@@ -1,19 +1,49 @@
+use std::{
+    str::FromStr,
+    sync::{Arc, Mutex},
+};
 
-use diesel_async::{pooled_connection::deadpool::Pool, AsyncPgConnection, RunQueryDsl};
+use crate::database::{
+    model::{NewUser, Users},
+    schema,
+};
 use diesel::{prelude::*, result::Error};
-use crate::database::{model::NewUser, schema};
+use diesel_async::{pooled_connection::deadpool::Pool, AsyncPgConnection, RunQueryDsl};
+use rand_chacha::{rand_core::RngCore, ChaCha8Rng};
 
-use super::error::DatabaseError;
+use super::{error::DatabaseError, model::Session};
 
-pub type Database = Pool<AsyncPgConnection>;
+type Database = Pool<AsyncPgConnection>;
+type Random = Arc<Mutex<ChaCha8Rng>>;
+pub struct SessionToken(u128);
+impl FromStr for SessionToken {
+    type Err = <u128 as FromStr>::Err;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        s.parse().map(Self)
+    }
+}
+impl SessionToken {
+    pub fn into_cookie_value(self) -> String {
 
+        self.0.to_string()
+    }
+    pub fn into_database_value(self) -> Vec<u8> {
+
+        self.0.to_be_bytes().to_vec()
+    }
+    pub fn generate_new(random: Random) -> Self {
+
+        let mut u128_pool = [0u8; 16];
+        random.lock().unwrap().fill_bytes(&mut u128_pool);
+        Self(u128::from_le_bytes(u128_pool))
+    }
+}
 pub async fn create_user(
     conn: &mut Database,
     username: String,
     passkey: String,
     email: Option<String>,
 ) -> Result<i32, diesel::result::Error> {
-
     let mut conn = conn.get().await.unwrap();
 
     let new_user = NewUser {
@@ -28,10 +58,87 @@ pub async fn create_user(
         .await;
     result
 }
-pub async fn delete_user(conn: &mut Database, usernam: String) -> Result<(), Error>{
+pub async fn delete_user_by(
+    conn: &mut Database,
+    usernam: String,
+) -> Result<(), diesel::result::Error> {
     let mut conn = conn.get().await.unwrap();
     use schema::users::dsl::*;
     diesel::delete(users.filter(username.like(usernam)))
         .execute(&mut conn)
-        .await.map_err(|_| DatabaseError::DeletionError)?
+        .await?;
+
+    Ok(())
 }
+pub async fn get_user_by(
+    conn: &mut Database,
+    usernam: String,
+) -> Result<Users, diesel::result::Error> {
+    use schema::users::dsl::*;
+    let mut conn = conn.get().await.unwrap();
+    let result = users
+        .filter(username.eq(usernam))
+        .select(Users::as_select())
+        .first(&mut *conn)
+        .await?;
+
+    Ok(result)
+}
+pub async fn get_all_users(conn: &mut Database) -> Result<Vec<Users>, diesel::result::Error> {
+    let mut conn = conn.get().await.unwrap();
+    use schema::users::dsl::*;
+    let all_users = users
+        .select(Users::as_select())
+        .get_results(&mut *conn)
+        .await?;
+
+    Ok(all_users)
+}
+pub async fn create_session(conn: &mut Database, token: SessionToken, uid: i32) -> Vec<u8> {
+    let mut conn = conn.get().await.unwrap();
+    let new_session = Session {
+        user_id: uid,
+        session_token: token.into_database_value(),
+    };
+    let result = diesel::insert_into(schema::session::table)
+        .values(&new_session)
+        .returning(schema::session::dsl::session_token)
+        .get_result::<Vec<u8>>(&mut *conn)
+        .await
+        .unwrap();
+    result
+}
+
+pub async fn get_id_pwd_by (conn: &mut Database, usernam: String) -> Option<(i32, String)> {
+
+    let mut conn = conn.get().await.unwrap();
+    use schema::users::dsl::*;
+    match users
+        .filter(username.eq(usernam))
+        .select((id, passkey))
+        .get_result::<(i32, String)>(&mut *conn)
+        .await
+    {
+        Ok((user_id, password)) => Some((user_id, password)),
+        Err(_) => None,
+    }
+}
+pub async fn delete_logged_in(
+    conn: &mut Database,
+    session_tk: SessionToken,
+) -> Result<usize, diesel::result::Error> {
+
+    let mut conn = conn.get().await.unwrap();
+    use schema::session::dsl::*;
+    use schema::users::dsl::*;
+    let target_user_id: i32 = session
+        .filter(session_token.eq(session_tk.into_database_value()))
+        .select(user_id)
+        .first::<i32>(&mut *conn)
+        .await?;
+
+    diesel::delete(users.filter(id.eq(target_user_id)))
+        .execute(&mut *conn)
+        .await
+}
+
